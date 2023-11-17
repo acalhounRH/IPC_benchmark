8e5c12618f5ccaf603f44fbf2f093d6420b5819c
Alexander Calhoun <31517635+acalhounRH@users.noreply.github.com>
2023-11-17 17:36:04 -0500

    Update ipc_benchmark.py
    modified to move latency calculation outside of ipc_worker

--- ipc_benchmark.py
+++ ipc_benchmark.py
@@ -16,13 +16,13 @@ try:
 except ImportError:
     posix_ipc = None
 
-def ipc_worker(data, process_id, message_size, message_pattern, args, latencies, mps, throughput, timestamps):
+def ipc_worker(data, process_id, message_size, message_pattern, args, timestamps):
     print("Creating ipc worker")
     
     num_messages = args.message_count
     messages_processed = 0
-    duration = args.duration
-
+    duration = args.duratio
+    
     if duration == 0 and num_messages == 0:
         raise ValueError("Both duration and num_messages cannot be 0. Specify a positive value for at least one of them.")
     Rstart_time = time.time()
@@ -39,17 +39,12 @@ def ipc_worker(data, process_id, message_size, message_pattern, args, latencies,
             response[:] = data[:message_size]
             end_time = time.time()
 
-            latency = end_time - start_time
-            latencies.append(latency)
-            mps_value = 1 / latency
-            mps.append(mps_value)
-            throughput_value = (message_size * 2) / (latency * 1024 * 1024)
-            throughput.append(throughput_value)
-            timestamps.append(end_time)
-
-            log_message = f"{datetime.utcfromtimestamp(timestamps[-1]).strftime('%Y-%m-%dT%H:%M:%S.%f')}," \
-                  f"{process_id},{latency:.6f},{mps_value:.2f},{throughput_value:.2f}"
-            logging.info(log_message)
+            timestamps.append({
+                'capture_time': datetime.utcfromtimestamp(end_time).strftime('%Y-%m-%dT%H:%M:%S.%f'),
+                'process_id': process_id,
+                'start_time': start_time,
+                'end_time': end_time
+            })
 
             Rend_time = time.time()
             if duration and (Rend_time - Rstart_time) >= duration:
@@ -68,17 +63,12 @@ def ipc_worker(data, process_id, message_size, message_pattern, args, latencies,
             data[:message_size] = message
             end_time = time.time()
 
-            latency = end_time - start_time
-            latencies.append(latency)
-            mps_value = 1 / latency
-            mps.append(mps_value)
-            throughput_value = (message_size) / (latency * 1024 * 1024)
-            throughput.append(throughput_value)
-            timestamps.append(end_time)
-            
-            log_message = f"{datetime.utcfromtimestamp(timestamps[-1]).strftime('%Y-%m-%dT%H:%M:%S.%f')}," \
-                  f"{process_id},{latency:.6f},{mps_value:.2f},{throughput_value:.2f}"
-            logging.info(log_message)
+            timestamps.append({
+                'capture_time': datetime.utcfromtimestamp(end_time).strftime('%Y-%m-%dT%H:%M:%S.%f'),
+                'process_id': process_id,
+                'start_time': start_time,
+                'end_time': end_time
+            })
             
             Rend_time = time.time()
             if duration and (Rend_time - Rstart_time) >= duration:
@@ -122,17 +112,34 @@ def run_ipc_benchmark(args):
         shared_data = data.buf[:args.data_size * 1024 * 1024]
 
     num_processes = args.process_count
-    latencies = multiprocessing.Manager().list()
-    mps = multiprocessing.Manager().list()
-    throughput = multiprocessing.Manager().list()
-    timestamps = multiprocessing.Manager().list()
+
 
     all_results = []
     all_agg_results = []
+    log_data = []
+    options = {
+        'Data Size (MB)': args.data_size,
+        'Duration (s)': args.duration if args.duration else 'Not Applicable',
+        'Message Count': args.message_count if args.message_count else 'Not Applicable',
+        'Log File': args.log_file,
+        'POSIX Shared Memory': args.posix,
+        'Message Size (bytes)': args.message_size,
+        'Message Pattern': args.message_pattern,
+        'Process Count': args.process_count,
+        'Output Format': 'Human-Readable' if args.human_readable else 'JSON',
+        'Runs': args.runs
+        }
 
     for run in range(args.runs):
+
+        latencies = multiprocessing.Manager().list()
+        mps = multiprocessing.Manager().list()
+        throughput = multiprocessing.Manager().list()
+        timestamps = multiprocessing.Manager().list()
         processes = []
+        
         start_run_time = time.time()
+        
         #for each process start a ipc worker
         for i in range(num_processes):
             process = multiprocessing.Process(target=ipc_worker, args=(shared_data, i, args.message_size, args.message_pattern, args, latencies, mps, throughput, timestamps))
@@ -143,8 +150,34 @@ def run_ipc_benchmark(args):
         for process in processes:
             process.join()
         end_run_time = time.time()
-        
         duration_runtime = end_run_time - start_run_time 
+
+        # Calculate latency, MPS, and throughput for each timestamp
+        for timestamp in timestamps:
+            latency = timestamp['end_time'] - timestamp['start_time']
+            latencies.append(latency)
+
+            mps_value = 1 / latency
+            mps.append(mps_value)
+
+            throughput_value = (args.message_size * 2) / (latency * 1024 * 1024)
+            throughput.append(throughput_value)
+                
+        # Log the calculated values for each run
+        for i, timestamp in enumerate(timestamps):
+            
+            log_data.append({
+                'capture_time': timestamp['capture_time'],
+                'process_id': timestamp['process_id'],
+                'latency': latencies[i],
+                'mps': mps[i],
+                'throughput': throughput[i],
+                'options': options
+            })
+            
+            log_message = f"{timestamp['capture_time']},{timestamp['process_id']},{latencies[i]:.6f},{mps[i]:.2f},{throughput[i]:.2f}"
+            logging.info(log_message)
+        
         latencies = list(latencies)
         p50_latency = np.percentile(latencies, 50)
         p90_latency = np.percentile(latencies, 90)
@@ -160,19 +193,6 @@ def run_ipc_benchmark(args):
 
         percent_deviation = np.std(latencies) / np.mean(latencies) * 100
         jitter = max(latencies) - min(latencies)
-
-        options = {
-            'Data Size (MB)': args.data_size,
-            'Duration (s)': args.duration if args.duration else 'Not Applicable',
-            'Message Count': args.message_count if args.message_count else 'Not Applicable',
-            'Log File': args.log_file,
-            'POSIX Shared Memory': args.posix,
-            'Message Size (bytes)': args.message_size,
-            'Message Pattern': args.message_pattern,
-            'Process Count': args.process_count,
-            'Output Format': 'Human-Readable' if args.human_readable else 'JSON',
-            'Runs': args.runs
-        }
 
         summary = {
             'Run': run + 1,  # Adding a "Run" counter
@@ -213,11 +233,6 @@ def run_ipc_benchmark(args):
     
             print("\nPercent Deviation: {:.3f}%".format(percent_deviation))
             print(f"Jitter: {jitter:.6f} seconds")
-    
-            #print("\nBenchmark Results:")
-            #print("Timestamp                    Process ID   Latency (s)   Msg/s    Throughput (MB/s)")
-            #for i in range(num_processes):
-            #    print(f"{datetime.utcfromtimestamp(timestamps[i]).strftime('%Y-%m-%dT%H:%M:%S.%f')}      {i}            {latencies[i]:.6f}       {mps[i]:.2f}    {throughput[i]:.2f}")
     
     shared_data = None
     shared_memory.close()
@@ -268,6 +283,10 @@ def run_ipc_benchmark(args):
     if args.output_json:
         with open('ipc_benchmark_aggregate_results.json', 'w') as json_file:
             json.dump(all_agg_results, json_file, indent=4)
+
+        # Dump log_data into JSON log file
+    with open('ipc_benchmark_log.json', 'w') as json_log_file:
+        json.dump(log_data, json_log_file, indent=4)
 
 def main():
     
